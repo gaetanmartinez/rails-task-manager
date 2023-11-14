39a20949353492e1c6fad60f41d128d4722e0fb3
gaetan martinez <gaetan.martinez81100@gmail.com>
2023-11-14 15:15:59 +0100

    Rails Task Manager exercise done

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,12 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
+  root "tasks#index"
+  post '/tasks/', to: 'tasks#create'
+  get '/tasks/new', to: 'tasks#new', as: 'new_task'
+  get '/tasks/:id', to: 'tasks#show', as: 'task'
+  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  patch '/tasks/:id', to: 'tasks#update'
+  delete 'tasks/:id', to: 'tasks#destroy'
   # root "posts#index"
 end
